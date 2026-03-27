0b4221a933eaaa3ac09f33851f9e1c0a7b099b0d
Phúc Duy <fenliboi@gmail.com>
2026-03-27 21:24:24 +0700

    add detail
    add detail page, search bar function,

--- utils/db.js
+++ utils/db.js
@@ -3,13 +3,13 @@ import knex from 'knex';
 const db = knex({
   client: 'mysql2',
   connection: {
-    host: 'localhost',      
-    port: 3306,            
-    user: 'root',           
-    password: '123456', 
-    database: 'librarymanagement' 
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '123456',
+    database: 'librarymanagement'
   },
-  pool: { min: 0, max: 10 } 
+  pool: { min: 0, max: 10 }
 });
 
 export default db;
