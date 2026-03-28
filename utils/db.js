8f19e55a3d0a539fddc69c1bdbee8c3ffa28b574
Hoang Thien <hoangthien312006@gmail.com>
2026-03-28 18:00:29 +0700

    chore(db): [PDHT] add dataset

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ const db = knex({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '123456',
+    password: '0909206693Thienhoangpd@#',
     database: 'librarymanagement'
   },
   pool: { min: 0, max: 10 }
